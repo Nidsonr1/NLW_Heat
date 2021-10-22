ad6a36e26e18bc0253fde7fb294c2b00bf0c254d
Nidsonr1 <nidsoncampos@hotmail.com>
2021-10-21 22:15:55 -0300

    Recebendo dados do Github Criada a migration de user

---
+++ src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { AuthenticateUserService } from '../services/AuthenticateUseSevice';
+
+
+class AuthenticateUserController {
+  async handle(req: Request, res: Response) {
+    const { code } = req.body;
+  
+    const service = new AuthenticateUserService();
+    const response = await service.execute(code);
+
+    return res.json(response);
+  }
+}
+
+export { AuthenticateUserController }
