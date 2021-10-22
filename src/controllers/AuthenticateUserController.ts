424b432dc884d081be42986b9c2e0d3738241f3d
Nidsonr1 <nidsoncampos@hotmail.com>
2021-10-22 20:59:16 -0300

    Ajustando retorno de erro da rota /Github

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -6,10 +6,13 @@ class AuthenticateUserController {
   async handle(req: Request, res: Response) {
     const { code } = req.body;
   
+   try {
     const service = new AuthenticateUserService();
     const response = await service.execute(code);
-
     return res.json(response);
+   } catch (err) {
+     return res.status(err.response.status).json({ error: err.message });
+   }
   }
 }
 
